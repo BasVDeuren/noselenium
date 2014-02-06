398e74f76a2a0b737d3ed7e46242b2c3b00dffa7
Atheesan Murgesu <m.atheesan@hotmail.com>
2014-02-07 00:08:31 +0100

    Webproject integrated in JavaProject

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -1,12 +1,12 @@
 /**
- * Created by Dimi on 3/02/14.
+ * Created by Atheesan on 3/02/14.
  */
 
 angular.module('spaceServices', ['ngResource'])
     .factory('Login', function ($resource) {
-        return $resource('http://localhost:8080/accesstokens')
+        return $resource('api/accesstokens')
     })
     .factory('Register', function ($resource) {
-        return $resource('rest/account')
+        return $resource('api/account')
     })
     ;
