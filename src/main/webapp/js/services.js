0361bd6aa503748b6d5d1fb346efdf46f44923c6
Atheesan Murgesu <m.atheesan@hotmail.com>
2014-02-18 14:45:21 +0100

    Update edit profile - frontend

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -2,15 +2,6 @@
  * Created by Atheesan on 3/02/14.
  */
 
-//angular.module('myApp.services', ['ngResource']).
-//    factory('CommentSvc', function ($resource) {
-//        return $resource('/cakephp/demo_comments/:action/:id/:page/:limit', { id:'@ID', 'page' : '@page', 'limit': '@limit' }, {
-//            'query' : { method: 'GET', params: { action : 'index' }, isArray: true },
-//            'save': { method: 'PUT', params: { action: 'edit' } },
-//            'add': { method: 'POST', params: { action: 'add' } },
-//            'delete': { method: 'DELETE', params: { action: 'delete' } } // since delete is reserved word, don't fight it just use remove instead
-//        });
-//    });
 
 angular.module('spaceServices', ['ngResource'])
     .factory('Login', function ($resource) {
