28b48ee3d51ee3438c98503af5708a698150669c
ArnoFlerackers <arno.flerackers@student.kdg.Be>
2014-02-19 09:11:28 +0100

    selenium test log test vervolledigd

--- src/test/java/be/kdg/spacecrack/seleniumtests/SeleniumLogOutTests.java
+++ src/test/java/be/kdg/spacecrack/seleniumtests/SeleniumLogOutTests.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class SeleniumLogOutTests extends SeleniumBaseTestCase {
 
     @Test
-    public void LogOutOK(){
+    public void LogOutOK() throws InterruptedException {
         login();
 
         WebElement btnAction = driver.findElement(By.name("btnAction"));
@@ -25,7 +25,9 @@ public class SeleniumLogOutTests extends SeleniumBaseTestCase {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login")));
 
         assertEquals("http://localhost:8080/#/", driver.getCurrentUrl());
-        //assertEquals(false, btnAction.isDisplayed());
+
+        Thread.sleep(2000);
+        assertEquals(false, btnAction.isDisplayed());
 
     }
 }
