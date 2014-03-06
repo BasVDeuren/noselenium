174b95a73960db95033958ee1f4bbad4ec4e925f
Atheesan Murgesu <m.atheesan@hotmail.com>
2014-03-07 00:03:26 +0100

    Facebook profile updated when user logs in - initial

--- src/main/webapp/js/controllers/LoginController.js
+++ src/main/webapp/js/controllers/LoginController.js
@@ -1,81 +1,114 @@
 /**
  * Created by Atheesan on 4/02/14.
  */
-function LoginController($scope, Login, Register, $cookieStore, Spinner) {
+function LoginController($scope, Login, Register, $cookieStore, Spinner, Contact) {
+
+    //data klaar zetten
+    $scope.contactData = {
+        firstname: "",
+        lastname: "",
+        dayOfBirth: "",
+        image: "",
+        email: ""
+    };
+
+    Contact.get(function (data) {
+        $scope.contactData.firstname = data.firstname;
+        $scope.contactData.lastname = data.lastname;
+        $scope.convertedDate.value = new Date(data.dayOfBirth);
+        $scope.contactData.image = data.image;
+        $scope.contactData.email = data.email;
+    }, function () {
+    });
+
+
     //Loading Spinner
-        $scope.startLoading = false;
-        $scope.loginData = {
-            email: "",
-            password: ""
-        };
-        $scope.hasLoginFailed = false;
-        $scope.alreadyRegistered = false;
-        $scope.login = function () {
-            Spinner.spinner.spin(Spinner.target);
-            Login.save($scope.loginData, function (data) {
-                Spinner.spinner.stop();
-                $cookieStore.put('accessToken', data.value);
-                $scope.go('/');
-                $scope.hasLoginFailed = false;
-            }, function () {
-                Spinner.spinner.stop();
-                $scope.hasLoginFailed = true;
-            });
+    $scope.startLoading = false;
+    $scope.loginData = {
+        email: "",
+        password: ""
+    };
+    $scope.hasLoginFailed = false;
+    $scope.alreadyRegistered = false;
+    $scope.login = function () {
+        Spinner.spinner.spin(Spinner.target);
+        Login.save($scope.loginData, function (data) {
+            Spinner.spinner.stop();
+            $cookieStore.put('accessToken', data.value);
+            $scope.go('/');
+            $scope.hasLoginFailed = false;
+        }, function () {
+            Spinner.spinner.stop();
+            $scope.hasLoginFailed = true;
+        });
 
-        };
+    };
 
-        $scope.validateLogin = function () {
-            return !($scope.loginData.email != '' && $scope.loginData.password != '');
-        };
+    $scope.validateLogin = function () {
+        return !($scope.loginData.email != '' && $scope.loginData.password != '');
+    };
 
-        $scope.fbLogin = function () {
+    $scope.fbLogin = function () {
 
-            FB.login(function (response) {
-                if (response.authResponse) {
-                    var user;
+        FB.login(function (response) {
+            if (response.authResponse) {
+                var user;
+                console.log(response);
+                FB.api('/me', function (response) {
                     console.log(response);
-                    FB.api('/me', function (response) {
-                        console.log(response);
-                        user = {
-                            email: response.email,
-                            password: 'facebook' + response.id
-                        };
-                        Spinner.spinner.spin(Spinner.target);
-                        Login.save(user, function (data) {
-                            Spinner.spinner.stop();
-                            $cookieStore.put('accessToken', data.value);
-                            $scope.go('/');
-                            $scope.hasLoginFailed = false;
-                        }, function () {
-                            Spinner.spinner.stop();
-                            $scope.registerFB(response);
-                        });
+                    user = {
+                        email: response.email,
+                        password: 'facebook' + response.id
+                    };
+                    Spinner.spinner.spin(Spinner.target);
+                    Login.save(user, function (data) {
+                        Spinner.spinner.stop();
+                        $cookieStore.put('accessToken', data.value);
+                        $scope.updateFbProfile();
+                        $scope.go('/');
+                        $scope.hasLoginFailed = false;
+                    }, function () {
+                        Spinner.spinner.stop();
+                        $scope.registerFB(response);
                     });
+                });
 
-                } else {
+            } else {
 //                    Spinner.spinner.stop();
-                    console.log('User cancelled login or did not fully authorize.');
-                }
-            }, {scope: 'email'});
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }, {scope: 'email'});
+    };
+
+    $scope.registerFB = function (response) {
+        Spinner.spinner.spin(Spinner.target);
+        var user = {
+            email: response.email,
+            username: response.name,
+            password: 'facebook' + response.id,
+            passwordRepeated: 'facebook' + response.id
         };
 
-        $scope.registerFB = function (response) {
-            Spinner.spinner.spin(Spinner.target);
-            var user = {
-                email: response.email,
-                username: response.name,
-                password: 'facebook' + response.id,
-                passwordRepeated: 'facebook' + response.id
-            };
+        Register.save(user, function (data) {
+            Spinner.spinner.stop();
 
-            Register.save(user, function (data) {
-                Spinner.spinner.stop();
-                $cookieStore.put('accessToken', data.value);
-                $scope.go('/');
-                $scope.alreadyRegistered = false;
-            }, function () {
-                Spinner.spinner.stop();
-                $scope.alreadyRegistered = true;
-            });
-        };
+            $cookieStore.put('accessToken', data.value);
+            $scope.updateFbProfile();
+            $scope.go('/');
+            $scope.alreadyRegistered = false;
+        }, function () {
+            Spinner.spinner.stop();
+            $scope.alreadyRegistered = true;
+        });
+    };
+
+    //Update profile with facebook data
+
+    $scope.updateFbProfile = function () {
+        $scope.contactData.dayOfBirth="1-1-1970";
+        $scope.contactData.firstname = "Samson";
+        Contact.save($scope.contactData, function () {
+        }, function () {
+        })
+    }
 }
