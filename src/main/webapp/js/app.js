7f61eba757812253024f666896e81f74520a0e2d
Atheesan Murgesu <m.atheesan@hotmail.com>
2014-03-14 18:19:45 +0100

    notifications updated (invited + messages)

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -141,12 +141,14 @@ spaceApp.controller("MainController", function ($scope, $cookies, $location, $ti
     //Invite Notifications
 
     $rootScope.invitesArray = [];
+    $rootScope.messagesArray = [];
     $rootScope.userInvites = [];
+    $rootScope.userMessages = [];
 
     $rootScope.loadInvites = function () {
         $rootScope.invitesArray = [];
         Profile.get(function (data) {
-            console.log("get api/auth/user");
+            //invites
             var firebaseUrl = 'https://vivid-fire-9476.firebaseio.com/invites/' + data.username.replace(/ /g, '') + data.profile.profileId;
             var ref = new Firebase(firebaseUrl);
             $rootScope.userInvites = $firebase(ref);
@@ -158,6 +160,18 @@ spaceApp.controller("MainController", function ($scope, $cookies, $location, $ti
                     }
                 }
             });
+
+            //messages
+            var firebaseMsgUrl = 'https://vivid-fire-9476.firebaseio.com/messages/' + data.username.replace(/ /g, '') + data.profile.profileId;
+            var refMsg = new Firebase(firebaseMsgUrl);
+            $rootScope.userMessages = $firebase(refMsg);
+
+            refMsg.on("child_added", function (dataSnapshot) {
+                if (!dataSnapshot.val().read) {
+                    $rootScope.messagesArray.push(dataSnapshot.val());
+                }
+            });
+
         }, function (data, headers) {
         });
     };
@@ -173,12 +187,24 @@ spaceApp.controller("MainController", function ($scope, $cookies, $location, $ti
                 }
             });
             $rootScope.invitesArray = [];
+            var firebaseUrl = 'https://vivid-fire-9476.firebaseio.com/messages/' + game.inviter + game.inviterId;
+            var ref = new Firebase(firebaseUrl);
+            $scope.allMessages = $firebase(ref);
+            $scope.allMessages.$add({sender: game.invited, senderId: game.invitedId,receiver: game.inviter,receiverId:game.inviterId, read: false, gameId: game.gameId});
             $scope.go('/spacecrack/game/' + game.gameId);
+            $rootScope.loadInvites();
         });
     };
 
     $scope.checkUnreadInvites = function () {
         if ($rootScope.invitesArray.length == 0) {
+            return false;
+        }
+        return true;
+    };
+
+    $scope.checkUnreadMessages = function () {
+        if ($rootScope.messagesArray.length == 0) {
             return false;
         }
         return true;
@@ -193,6 +219,17 @@ spaceApp.controller("MainController", function ($scope, $cookies, $location, $ti
 
         $rootScope.invitesArray = [];
     };
+
+    $scope.setMessagedAsRead = function () {
+        var keys = $rootScope.userMessages.$getIndex();
+        keys.forEach(function (key, i) {
+            $rootScope.userMessages[key].read = true;
+            $rootScope.userMessages.$save(key);
+        });
+
+        $rootScope.messagesArray = [];
+    };
+
     Profile.get(function (data) {
         console.log("get api/auth/user");
         loginData.password = data.password;
